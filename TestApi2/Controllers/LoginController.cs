05c6f81711e76dcb712347ecf42d510c69ffc935
David Peralta <19470212@campeche.tecnm.mx>
2023-05-10 17:12:06 -0600

    Agregar archivos de proyecto.

---
+++ TestApi2/Controllers/LoginController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using TestApi2.Models;
+
+namespace TestApi2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : Controller
+    {
+        private IConfiguration _config;
+
+        public LoginController(IConfiguration config)
+        {
+            _config = config;
+        }
+        [AllowAnonymous]
+        [HttpPost]
+        public IActionResult Login([FromBody] Login login)
+        {
+            IActionResult response = Unauthorized();
+            var user = AuthenticateUser(login);
+
+            if (user != null)
+            {
+                var tokenString = GenerateJSONWebToken(user);
+                response = Ok(new { token = tokenString });
+            }
+
+            return response;
+        }
+
+        private string GenerateJSONWebToken(Login userInfo)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var claims = new[] {
+                new Claim(JwtRegisteredClaimNames.Sub, userInfo.name),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+            };
+
+            var token = new JwtSecurityToken(_config["Jwt:Issuer"],
+                _config["Jwt:Issuer"],
+                claims,
+                expires: DateTime.Now.AddMinutes(120),
+                signingCredentials: credentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        private Login AuthenticateUser(Login login)
+        {
+            Login user = null;
+
+            if (login.name == "Usuario")
+            {
+                user = new Login { name = "Admin" };
+            }
+            return user;
+        }
+    }
+}
---
+++ TestApi2/Models/Login.cs
@@ -0,0 +1,9 @@
+﻿namespace TestApi2.Models
+{
+    public class Login
+    {
+        public int? Id { get; set; }
+        public string? name { get; set; }
+        public string? pass { get; set; }
+    }
+}
